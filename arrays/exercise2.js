840f8ce506448b181029bd4835c7f164047bc130
zachee <zacheeishimwe@gmail.com>
2021-04-08 11:25:28 +0200

    add exercises 2

---
+++ arrays/exercise2.js
@@ -0,0 +1,33 @@
+// merge sorted arrays into one array
+
+function mergeSortedArrays(arr1, arr2){
+    const mergedArray = [];
+    arr1Item = arr1[0];
+    arr2Item = arr2[0];
+    let i =1;
+    let j =1;
+    // check for the inputs 
+
+    if(arr1.length===0){
+        return arr2;
+    }
+
+    if(arr2.length===0){
+        return arr1;
+    }
+
+    while(arr1Item || arr2Item){
+        if(!arr2Item || arr1Item < arr2Item){
+           mergedArray.push(arr1Item);
+           arr1Item = arr1[i];
+           i++
+        }else{
+            mergedArray.push(arr2Item);
+            arr2Item = arr2[j];
+            j++;
+        }
+    }
+    return mergedArray;
+}
+
+console.log(mergeSortedArrays([0,3,4,31],[4,6,30]))
