ae5212dfea8741885f3ae09e77d2a8a3bc136f03
zachee <zacheeishimwe@gmail.com>
2021-04-13 12:55:38 +0200

    add append method on linkedList

--- linkeList/linkedList.js
+++ linkeList/linkedList.js
@@ -1,21 +1,3 @@
-// how js going to identify a linkedList 
-// 10-->5-->16-->10
-
-// let MyLinkedList = {
-//     value:10,
-//     next:{
-//         value:5,
-//         next:{
-//             value:16,
-//             next:{
-//                 value:10,
-//                 next:null
-//             }
-//         }
-//     }
-// } 
-
-
 class LinkedList {
   constructor(value){
      this.head = {
@@ -25,8 +7,23 @@ class LinkedList {
      this.tail = this.head;
      this.length = 1;
   }
+  
+  append(value){
+  const newNode ={
+      value:value,
+      next:null
+  }
+  this.tail.next = newNode;
+  this.tail = newNode;
+  this.length++;
+  return this;
+  }
 }
 
 const myLinkedList = new  LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(20);
 
 console.log(myLinkedList)
+
