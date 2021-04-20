6a48d9fca778fda9ccc18ea8a584086d5813eb3b
zachee <zacheeishimwe@gmail.com>
2021-04-20 12:48:00 +0200

    add reverse method

--- linkeList/singleLinkedList.js
+++ linkeList/singleLinkedList.js
@@ -1,97 +1,110 @@
-class Node{
-  constructor(value){
-    this.value = value,
-    this.next  = null
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
   }
 }
 
 class LinkedList {
-  constructor(value){
-     this.head = {
-         value:value,
-         next:null
-     }
-     this.tail = this.head;
-     this.length = 1;
-  }
-  
-  append(value){
-  const newNode = new Node(value)
-  this.tail.next = newNode;
-  this.tail = newNode;
-  this.length++;
-  return this;
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
 
-  prepend(value){
-    const newNode = new Node(value)
-    newNode.next = this.head
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
   }
 
-  PrintList(){
+  PrintList() {
     const arr = [];
-    let headNode = this.head;
+    let currentNode = this.head;
 
-    while(headNode !== null){
-      arr.push(headNode.value);
-      headNode = headNode.next;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
     }
-    return arr
+    return arr;
   }
 
-  insert(index,value){
-    // check the params 
-    if(index>=this.length){
-      return this.append(value);;
+  insert(index, value) {
+    // check the params
+    if (index >= this.length) {
+      return this.append(value);
     }
-    if(index===0){
+    if (index === 0) {
       this.prepend(value);
       return this.PrintList();
     }
-  const newNode = new Node(value);
-    const leader = this.traverseToIndex(index-1);
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
     const holdingPointer = leader.next;
     leader.next = newNode;
     newNode.next = holdingPointer;
     this.length++;
-    return this.PrintList()
+    return this.PrintList();
   }
 
-    traverseToIndex(index){
-      // check params 
-      let counter = 0;
-      let currentNode = this.head;
-      while(counter !== index){
-        currentNode = currentNode.next;
-        counter++;
-      }
-      return currentNode;
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
     }
-
-    remove(index){
-      // check the parameters 
-      const leader =  this.traverseToIndex(index-1);
-      const unwantedNode = leader.next;
-      leader.next = unwantedNode.next;
-      this.length--;
-      return this.PrintList();
-    }
-
+    return currentNode;
   }
 
+  remove(index) {
+    // check the parameters
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.PrintList();
+  }
 
+  reverse() {
+    // check the inputs [1,10,16,88]
+    if (this.length === 1) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      let temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.PrintList();
+  }
+}
 
-const myLinkedList = new  LinkedList(10);
+const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.append(20);
 myLinkedList.prepend(1);
-myLinkedList.insert(200,100);
-myLinkedList.insert(2,99);
-myLinkedList.remove(2)
-
-console.log(myLinkedList.PrintList())
-
+myLinkedList.insert(200, 100);
+myLinkedList.insert(2, 99);
+myLinkedList.remove(2);
+console.log(myLinkedList.reverse());
